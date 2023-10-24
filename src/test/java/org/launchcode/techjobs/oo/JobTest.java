c2791061398d9a816b6f36a4ba45a4c5842361fb
Chad Harper <chadharper811@gmail.com>
2023-10-23 20:51:46 -0500

    Finished first 3 tasks completely starting on task 4 - unit tests

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,19 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job test_job1 = new Job("name", new Employer("emp"), new Location("loc"), new PositionType("pos"), new CoreCompetency("core"));
+        Job test_job2 = new Job("name", new Employer("emp"), new Location("loc"), new PositionType("pos"), new CoreCompetency("core"));
+        assertNotEquals(test_job1, test_job2);
+    }
+
 }
